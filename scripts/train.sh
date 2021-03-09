01ce73d83c413f84240b2d925a76949c2b2b0e02
octavmatu <florinoctavian.matu@gmail.com>
2021-03-09 19:04:50 +0200

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -8,7 +8,12 @@ fi
 #if [ FLAG_S3_DOWNLOAD -gt 0 ]; then
 echo "Getting data from S3"
 aws s3 sync s3://${S3_BUCKET_NAME}/cfg cfg/
-aws s3 sync s3://${S3_BUCKET_NAME}/data data/
+aws s3 sync s3://${S3_BUCKET_NAME}/data/obj.names data/
+aws s3 sync s3://${S3_BUCKET_NAME}/data/obj.data data/
+aws s3 sync s3://${S3_BUCKET_NAME}/data/test.txt data/
+aws s3 sync s3://${S3_BUCKET_NAME}/data/train.txt data/
+aws s3 sync s3://${S3_BUCKET_NAME}/data/images data/images
+aws s3 sync s3://${S3_BUCKET_NAME}/data/labels data/labels
 aws s3 sync s3://${S3_BUCKET_NAME}/pretrained pretrained/
 #fi
 
