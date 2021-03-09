5e2e266fca41f12c3b0e5591bc21e0b7670cf86d
octavmatu <florinoctavian.matu@gmail.com>
2021-03-09 18:40:25 +0200

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -5,12 +5,12 @@ if [ ! -d "backup" ]; then
     mkdir backup
 fi
 
-if [ FLAG_S3_DOWNLOAD -gt 0 ]; then
-    echo "Getting data from S3"
-    aws s3 sync s3://${S3_BUCKET_NAME}/cfg cfg/
-    aws s3 sync s3://${S3_BUCKET_NAME}/data data/
-    aws s3 sync s3://${S3_BUCKET_NAME}/pretrained pretrained/
-fi
+#if [ FLAG_S3_DOWNLOAD -gt 0 ]; then
+echo "Getting data from S3"
+aws s3 sync s3://${S3_BUCKET_NAME}/cfg cfg/
+aws s3 sync s3://${S3_BUCKET_NAME}/data data/
+aws s3 sync s3://${S3_BUCKET_NAME}/pretrained pretrained/
+#fi
 
 if [ -f "cfg/${NETWORK_FILENAME}" ]; then
     export NETWORK_CONFIG="cfg/${NETWORK_FILENAME}"
