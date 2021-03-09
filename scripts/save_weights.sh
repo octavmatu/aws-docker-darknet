05b483b5820b42c719c3677aebe5b94623460550
octavmatu <florinoctavian.matu@gmail.com>
2021-03-09 18:03:39 +0200

    Update save_weights.sh

--- scripts/save_weights.sh
+++ scripts/save_weights.sh
@@ -10,3 +10,4 @@ BASE_NAME=${NETWORK_FILENAME%.cfg}
 echo "Uploading final weights to s3://${S3_BUCKET_NAME}/models/${BASE_NAME}_${UNIX_EPOCH}.weights"
 
 aws s3 cp backup/${BASE_NAME}_final.weights s3://${S3_BUCKET_NAME}/models/${BASE_NAME}_${UNIX_EPOCH}.weights
+aws s3 cp backup/${BASE_NAME}_best.weights s3://${S3_BUCKET_NAME}/models/${BASE_NAME}_${UNIX_EPOCH}_best.weights
